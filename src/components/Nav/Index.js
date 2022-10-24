7bdc9372660dceeaa71687685d62091e135b7dae
tefinirinasarobidy <fanomezantsoatefinirina@gmail.com>
2022-10-24 17:13:55 +0200

    nav bar

--- src/components/Nav/Index.js
+++ src/components/Nav/Index.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import { useDispatch } from 'react-redux';
-import './Index.css'
+import React from "react";
+import { useDispatch } from "react-redux";
+import "./Index.css";
 import { token } from "../../store/actions";
 
 export default function Index() {
-  const dispatch = useDispatch()
-    const deconnecter = () => {
-        localStorage.removeItem('token');
-        dispatch(token(null))
-    }
+  const dispatch = useDispatch();
+  const deconnecter = () => {
+    localStorage.removeItem("token");
+    dispatch(token(null));
+  };
   return (
-    <div className='nav'>
-        <div className='left'>Lorem </div>
-        <div className='right'>
-            <span className='button'>Message</span>
-            <span className='button'>Notification</span>
-            <span className='button' onClick={deconnecter}>Deconnecter</span>
-        </div>
+    <div className="nav">
+      <div className="logo"> logo</div>
+      <div className="action">
+        <ul>
+          <li>
+            <a href="">Commander</a>
+          </li>
+          <li>
+            <a href="">Commander</a>
+          </li>
+          <li>
+            <a href="">Devenir chauffeur</a>
+          </li>
+          <li>
+            <a href="">créer un compte</a>
+          </li>
+          <li className="register">
+            <a href="">Se connecter</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
